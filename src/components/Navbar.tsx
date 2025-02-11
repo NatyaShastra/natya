d2004eef5c813f3eb937fd70c19af2ab9b123389
NatyaShastra <natyashastra2018@gmail.com>
2025-02-12 00:57:08 +0530

    Mobile friendly styles

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,25 +8,15 @@ export const Navbar: React.FC = () => {
 
   return (
     <nav className="navbar">
-      <div className="nav-container">
-        <Link to="/" className="nav-logo">
-        <img src={logo} alt="NatyaShastra Logo" className="header-logo" />
-        </Link>
-        <div className="menu-icon" onClick={() => setMenuOpen(!menuOpen)}>
-          <div className={menuOpen ? "bar open" : "bar"}></div>
-          <div className={menuOpen ? "bar open" : "bar"}></div>
-          <div className={menuOpen ? "bar open" : "bar"}></div>
-        </div>
-        <ul className={menuOpen ? "nav-links open" : "nav-links"}>
-          <li><Link to="/" onClick={() => setMenuOpen(false)}>Home</Link></li>
-          <li><Link to="/enquiry" onClick={() => setMenuOpen(false)}>Enquiry</Link></li>
-          <li><Link to="/onboarding" onClick={() => setMenuOpen(false)}>Onboard</Link></li>
-          <li><Link to="/booking" onClick={() => setMenuOpen(false)}>Order</Link></li>
-          <li><Link to="/about" onClick={() => setMenuOpen(false)}>Contact</Link></li>
-          {/* <li><Link to="/services" onClick={() => setMenuOpen(false)}>Services</Link></li>
-          <li><Link to="/contact" onClick={() => setMenuOpen(false)}>Contact</Link></li> */}
-        </ul>
-      </div>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/enquiry">Enquiry</Link></li>
+        <li><Link to="/onboarding">Onboard</Link></li>
+        <li><Link to="/booking">Place Orders</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/services">Services</Link></li>
+        <li><Link to="/contact">Contact</Link></li>
+      </ul>
     </nav>
   );
 };
