b6afba67b1518b4b792198ddc6bc81214ef4258f
NatyaShastra <natyashastra2018@gmail.com>
2025-02-11 22:46:06 +0530

    All code chagnes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -22,9 +22,9 @@ export const Navbar: React.FC = () => {
           <li><Link to="/enquiry" onClick={() => setMenuOpen(false)}>Enquiry</Link></li>
           <li><Link to="/onboarding" onClick={() => setMenuOpen(false)}>Onboard</Link></li>
           <li><Link to="/booking" onClick={() => setMenuOpen(false)}>Order</Link></li>
-          <li><Link to="/about" onClick={() => setMenuOpen(false)}>About</Link></li>
-          <li><Link to="/services" onClick={() => setMenuOpen(false)}>Services</Link></li>
-          <li><Link to="/contact" onClick={() => setMenuOpen(false)}>Contact</Link></li>
+          <li><Link to="/about" onClick={() => setMenuOpen(false)}>Contact</Link></li>
+          {/* <li><Link to="/services" onClick={() => setMenuOpen(false)}>Services</Link></li>
+          <li><Link to="/contact" onClick={() => setMenuOpen(false)}>Contact</Link></li> */}
         </ul>
       </div>
     </nav>
