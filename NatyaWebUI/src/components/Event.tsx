8d766a38fbee53e664c51bf65e64579bd4fb5f29
NatyaShastra <natyashastra2018@gmail.com>
2026-02-21 13:01:47 +0000

    event details updated

--- NatyaWebUI/src/components/Event.tsx
+++ NatyaWebUI/src/components/Event.tsx
@@ -3,29 +3,67 @@ import "../styles/EventGallery.css";
 
 export const Event: React.FC = () => (
   <div className="event-bg">
-    <div className="event-content">
-      <h2>Completed Events</h2>
-      <ul>
-        <li>
-          <h2><strong>2025 Salangai & NatyaShastra 9th Annual Day Celebration</strong></h2>
-          <div>Salangai Students: <strong>Hiya, Moksha, Anivitha, Sri Saanvi</strong></div>
-          <div>Venue: Preston Prime Mall, Gachibowli, Hyderabad</div>
-          <div>
-            Highlights: <a href="https://www.youtube.com/watch?v=j6bXFoU9RNc" target="_blank" rel="noopener noreferrer">Watch Highlights</a><br />
-            Full Video: <a href="https://www.youtube.com/watch?v=8TONd98ippA&t=6354s" target="_blank" rel="noopener noreferrer">Watch Full Event</a><br />
-            More details: <a href="https://docs.google.com/spreadsheets/d/1EvOy75xYUpiKmKyBkCwcthnsWNVV4N43/edit?gid=1636669978#gid=1636669978" target="_blank" rel="noopener noreferrer">Participation & Details</a>
+    <div className="event-content"> 
+      <div className="event-columns">
+        <div className="event-list">
+          <ul>
+            <li>
+              <h2><strong>2026 Salangai & NatyaShastra 10th Annual Day celebration</strong></h2>
+              <div>Details coming soon.</div>
+            </li>
+            <li>
+              <h2><strong>2025 Salangai & NatyaShastra 9th Annual Day Celebration</strong></h2>
+              <div>Salangai Students: <strong>Hiya, Moksha, Anivitha, Sri Saanvi</strong></div>
+              <div>Venue: Preston Prime Mall, Gachibowli, Hyderabad</div>
+              <div>
+                Highlights: <a href="https://www.youtube.com/watch?v=j6bXFoU9RNc" target="_blank" rel="noopener noreferrer">Watch Highlights</a><br />
+                Full Video: <a href="https://www.youtube.com/watch?v=8TONd98ippA&t=6354s" target="_blank" rel="noopener noreferrer">Watch Full Event</a><br />
+                Invitation: <a href="https://www.instagram.com/natyashastraforall/reel/DKNOsP_P7QY/" target="_blank" rel="noopener noreferrer">Invitation</a><br />
+                Blooper: <a href="https://www.facebook.com/share/r/1DbgTeQMPr/" target="_blank" rel="noopener noreferrer">Blooper</a><br />
+                More details: <a href="https://docs.google.com/spreadsheets/d/1EvOy75xYUpiKmKyBkCwcthnsWNVV4N43/edit?gid=1636669978#gid=1636669978" target="_blank" rel="noopener noreferrer">Participation & Details</a>
+                <br />
+                📸 Photo Albums:
+                <br />
+                <a href="https://drive.google.com/drive/folders/1EblRp25Yp4D4aYwPvACAktKYe-lBjXjm?usp=sharing" target="_blank" rel="noopener noreferrer">Salangai 2025 Pics & Videos (Album 1)</a>
+                <br />
+                <a href="https://photos.app.goo.gl/vyWK76TUuXx3TDmc6" target="_blank" rel="noopener noreferrer">Salangai 2025 Pics & Videos (Album 2)</a>
+                <br />
+                <a href="https://photos.google.com/share/AF1QipOkw7T0Wy99xmHGLasz1CHrjwuIV2Vr_Juak9Zg4ojHIicg1ABD8W5HGQZMltJ66A?key=OXFENnUtMlc4b3NwYW5OQS1QNTJ0aHVELVNvNTJn" target="_blank" rel="noopener noreferrer">Salangai 2025 Pics & Videos (Album 3)</a>
+                <br />
+                <a href="https://photos.app.goo.gl/fEXQwtANBjAY8C256" target="_blank" rel="noopener noreferrer">Salangai 2025 Pics & Videos (Album 4)</a>
+                <br />
+                <a href="https://photos.app.goo.gl/967HZsiFzzaB3PV19" target="_blank" rel="noopener noreferrer">Salangai 2025 Pics & Videos (Album 5)</a>
+                <br />
+             </div>
+            </li>
+            <li>
+              <h2><strong>2024 Salangai & NatyaShastra 8th Annual Day Celebration</strong></h2>
+              <div>Salangai Students: <strong>Neha, Amaya, Hitakshi</strong></div>
+              <div>Venue: Preston Prime Mall, Gachibowli, Hyderabad</div>
+              <div>
+                Highlights: <a href="https://www.youtube.com/watch?v=z4bqEwFoWRE" target="_blank" rel="noopener noreferrer">Watch Highlights</a><br />
+                Full Video: <a href="https://www.youtube.com/watch?v=fLo7PHAv70U&t=6647s" target="_blank" rel="noopener noreferrer">Watch Full Event</a>
+                <br />
+                Invitation: <a href="https://www.instagram.com/p/C78cawnhKyA/" target="_blank" rel="noopener noreferrer">Invitation</a><br />
+                Blooper: <a href="https://www.instagram.com/natyashastraforall/reel/DIeY87QBztX/" target="_blank" rel="noopener noreferrer">Blooper</a>
+                <br />
+                📸 Photo Albums:
+                <br />
+                <a href="https://photos.google.com/share/AF1QipM0g4P8tlRoLBbcUp-aI-ieIgocQEQafOGpgef8eLsRxqq9GN7wVWWaBE9A37EQ_Q?key=eVhvbDl4YjJwUkJrbEJuZ19SZnZ6THpyVHlFRlp3" target="_blank" rel="noopener noreferrer">Salangai 2024 pics</a>
+              </div>
+            </li>
+          </ul>
+        </div>
+        <aside className="event-side">
+          <h3>Event Links</h3>
+          <div style={{marginTop:8}}>
+            Event Registration: <a href="https://docs.google.com/forms/d/11RJkLRD7Dca4lIHYkiWZWA9IVc-I2wVwC-8fhk0O9RE/edit" target="_blank" rel="noopener noreferrer">Register</a>
           </div>
-        </li>
-        <li>
-          <h2><strong>2024 NatyaShastra Annual Day & Salangai Pooja</strong></h2>
-          <div>Salangai Students: <strong>Neha, Amaya, Hitakshi</strong></div>
-          <div>Venue: Preston Prime Mall, Gachibowli, Hyderabad</div>
-          <div>
-            Highlights: <a href="https://www.youtube.com/watch?v=z4bqEwFoWRE" target="_blank" rel="noopener noreferrer">Watch Highlights</a><br />
-            Full Video: <a href="https://www.youtube.com/watch?v=fLo7PHAv70U&t=6647s" target="_blank" rel="noopener noreferrer">Watch Full Event</a>
+          <div style={{marginTop:8}}>
+            Attendees information: <a href="https://docs.google.com/forms/d/1_Q-X7_Nf8CL89JViK5Oo2wixAaj3IkCatLmRgMMEwEE/edit" target="_blank" rel="noopener noreferrer">Attendees Info</a>
           </div>
-        </li>
-      </ul>
+        </aside>
+      </div>
     </div>
   </div>
 );
